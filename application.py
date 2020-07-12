616a823e4fb2abc580546f439d4038debce08b39
rukaiyaanwar <rukaiyaanwar@users.noreply.github.com>
2020-07-12 22:35:49 +0530

    Password hashing and verification

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from wtforms_fields import *
 from ext import db
+from passlib.hash import pbkdf2_sha256
 from models import *
 
 app = Flask(__name__)
@@ -23,9 +24,13 @@ def index():
         username = reg_form.username.data
         password = reg_form.password.data
 
+        #Hash of the password
+
+        hashed_pswd = pbkdf2_sha256.hash(password)
+
         # Add user to DB  
 
-        user = User(username=username, password=password)
+        user = User(username=username, password=hashed_pswd)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
